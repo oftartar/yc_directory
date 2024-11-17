77532bf79cc633fc1d73ef4062d528014d31721a
oftartar <oftartar@gmail.com>
2024-11-17 23:02:23 +0300

    implement user profile page

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -6,7 +6,21 @@ export const STARTUP_QUERY = defineQuery(`
   title,
   _createdAt,
   author -> {
-    _id, name
+    _id, name, image
+  },
+  views,
+  description,
+  category,
+  image
+}`);
+
+export const STARTUPS_BY_AUTHOR_QUERY = defineQuery(`
+    *[_type == 'startup' && author._ref == $id] | order(_createdAt desc) {
+  _id,
+  title,
+  _createdAt,
+  author -> {
+    _id, name, image
   },
   views,
   description,
@@ -38,6 +52,17 @@ export const STARTUP_VIEWS_QUERY = defineQuery(`
 
 export const AUTHOR_BY_GITHUB_ID_QUERY = defineQuery(`
     *[_type == 'author' && id == $id ][0] {
+  _id,
+  id,
+  name,
+  username,
+  email,
+  image,
+  bio
+}`);
+
+export const AUTHOR_BY_ID_QUERY = defineQuery(`
+    *[_type == 'author' && _id == $id ][0] {
   _id,
   id,
   name,
